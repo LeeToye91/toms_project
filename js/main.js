5d61173cdd573d12f7d4b1c61a23fe6ec2f8bfde
Tom Wheeler <twheeler1@live.co.uk>
2024-07-23 13:40:14 +0100

    first commit

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 
 // Just showing you how your javascript can manipulate your HTML. Look for javascriptTest in your index.html file
-// If you want to stop the functionality simply comment out ALL the code below. 
+// If you want to stop the functionality simply comment out ALL the code below.
 
-let i = 0;
-function change() {
-  const doc = document.getElementById("javascriptTest");
-  const color = ["white", "blue", "brown", "green"];
-  doc.style.color = color[i];
-  i = (i + 1) % color.length;
-}
-setInterval(change, 1000); 
+//let i = 0;
+//function change() {
+  //const doc = document.getElementById("javascriptTest");
+ // const color = ["white", "blue", "brown", "green"];
+ // doc.style.color = color[i];
+ // i = (i + 1) % color.length;
+//}
+//setInterval(change, 1000); 
